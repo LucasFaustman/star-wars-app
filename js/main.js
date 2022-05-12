c3ba563a9e2777f7d10bce4b28495ad8e87b5636
Lucas Faustman <lucasfaustman@gmail.com>
2022-05-12 16:26:20 -0700

    Got the carousel to partially work

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@ function flipCard() {
 card.forEach((card) => card.addEventListener('click',flipCard))
 
 document.addEventListener('click', e => {
-
-
 // slider
 let handle 
 if (e.target.matches('.handle'))  {
@@ -19,41 +17,42 @@ if (e.target.matches('.handle'))  {
 } else {
   handle = e.target.closest('.handle')
 } 
-if (handle != null) {
+if (handle != null) 
   onHandleClick(handle)
-}
+
 })
 
 function onHandleClick (handle) {
-  const slider = handle.closest('body').querySelector('.cards')
-  const sliderIndex = parseInt(slider.style.getPropertyValue('--cards=index'))
-  if (handle.classList.contains('.left-handle')) {
-    slider.style.setProperty('--cards-index', sliderIndex - 1)
+  const cards = handle.closest('body').querySelector('.cards')
+  const cardsIndex = parseInt(getComputedStyle(cards).getPropertyValue('--cards-index'))
+  if (handle.classList.contains('left-handle')) {
+    cards.style.setProperty('--cards-index', cardsIndex - 1)
   }
 
-  if (handle.classList.includes('.right-handle')) {
-    slider.style.setProperty('--cards=index', sliderIndex + 1)
+  if (handle.classList.contains('right-handle')) {
+    cards.style.setProperty('--cards-index', cardsIndex + 1)
   }
+  console.log(cards)
 }
 
 
 
 
-document.querySelector('button').addEventListener('click', getFetch)
+// document.querySelector('button').addEventListener('click', getFetch)
 
-function getFetch(){
-  const choice = document.querySelector('input').value
-  console.log(choice)
-  const url = `https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id/1.json`
+// function getFetch(){
+//   const choice = document.querySelector('input').value
+//   console.log(choice)
+//   const url = `https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id/1.json`
 
-  fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        document.querySelector('h1').innerHTML = data.homeworld.charAt(0).toUpperCase()+data.homeworld.slice(1)
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
-}
+//   fetch(url)
+//       .then(res => res.json()) // parse response as JSON
+//       .then(data => {
+//         console.log(data)
+//         document.querySelector('h1').innerHTML = data.homeworld.charAt(0).toUpperCase()+data.homeworld.slice(1)
+//       })
+//       .catch(err => {
+//           console.log(`error ${err}`)
+//       });
+// }
 
